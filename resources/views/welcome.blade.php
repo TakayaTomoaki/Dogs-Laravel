6307b21ef950fed820ec2a6cc1e905f87312354a
TakayaTomoaki <t.takaya55@gmail.com>
2021-08-16 10:58:21 +0900

    「Laravel/ui」の追加、UIを「Vue」に設定

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dogs
                 </div>
 
                 <div class="links">
