3e395eadeb76f25f4523c70db7f1db67e91d28d3
takayatomoaki <t.takaya55@gmail.com>
2021-09-28 08:28:01 +0900

    [追加]shareの投稿・保存 : ~/home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Share;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -30,4 +32,29 @@ class HomeController extends Controller
         $user_id = Auth::id();
         return view('home', ['user_id' => $user_id]);
     }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $post = $request->post();
+        if ($post['body'] === null) {
+            return back();
+        }
+
+        $user_id = Auth::id();
+        $share = new Share();
+
+        if (!empty($post['image'])) {
+            $path = $post['image']->store('public/image');
+            $share->image = basename($path);
+        } else {
+            $share->image = null;
+        }
+
+        $share->user_id = $user_id;
+        $share->body = $post['body'];
+        $share->save();
+
+        return back();
+    }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,48 +7,48 @@
                 <div class="card m-b-md">
                     <div class="card-header">ホーム</div>
                     <div class="card-body">
-                        <form action="{{ action('ShareController@create') }}" method="post" enctype="multipart/form-data">
-
-                            @if (count($errors) > 0)
-                                <ul>
-                                    @foreach($errors->all() as $e)
-                                        <li>{{ $e }}</li>
-                                    @endforeach
-                                </ul>
-                            @endif
-
+                        @if (count($errors) > 0)
+                            <ul>
+                                @foreach($errors->all() as $e)
+                                    <li>{{ $e }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+                        <form action="{{ action('HomeController@store') }}" method="post"
+                              enctype="multipart/form-data">
                             <div class="form-group row">
                                 <label class="col-md-2">シェア</label>
-                                <div class="col-md-10">
-                                    <input type="text" class="form-control" name="share" value="{{ old('share') }}">
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="body"
+                                           value="{{ old('body') }}">
                                 </div>
                             </div>
-
                             <div class="form-group row">
                                 <label class="col-md-2">画像</label>
                                 <div class="d-flex">
-                                    <div class="col-md-12">
+                                    <div class="col-md-8">
                                         <input type="file" class="form-control-file" name="image">
                                     </div>
-                                </div>
-                                {{ csrf_field() }}
-                                <div class="justify-content-end">
-                                    <input type="submit" class="btn btn-primary" value="シェアする">
+                                    {{ csrf_field() }}
+                                    <div class="justify-content-end">
+                                        <input type="submit" class="btn btn-primary" value="シェアする">
+                                    </div>
                                 </div>
                             </div>
                         </form>
                     </div>
-                    <div class="card m-b-md">
-                        <div class="card-header">シェア一覧</div>
-                        <div class="card-body">
-                            <div class="links">
-                                <a href="{{ route( 'home') }}">ホーム</a><br>
-                                <a href="{{ route( 'mypage', ['user_id' => $user_id]) }}">マイページ</a><br>
-                                <a href="{{ route( 'search') }}">検索</a><br>
-                                <a href="{{ route( 'notice', ['user_id' => $user_id]) }}">通知</a><br>
-                                <a href="{{ route( 'messages', ['user_id' => $user_id]) }}">メッセージ</a><br>
-                                <a href="{{ route( 'setup', ['user_id' => $user_id]) }}">設定</a>
-                            </div>
+                </div>
+
+                <div class="card m-b-md">
+                    <div class="card-header">シェア一覧</div>
+                    <div class="card-body">
+                        <div class="links">
+                            <a href="{{ route( 'home') }}">ホーム</a><br>
+                            <a href="{{ route( 'mypage', ['user_id' => $user_id]) }}">マイページ</a><br>
+                            <a href="{{ route( 'search') }}">検索</a><br>
+                            <a href="{{ route( 'notice', ['user_id' => $user_id]) }}">通知</a><br>
+                            <a href="{{ route( 'messages', ['user_id' => $user_id]) }}">メッセージ</a><br>
+                            <a href="{{ route( 'setup', ['user_id' => $user_id]) }}">設定</a>
                         </div>
                     </div>
                 </div>
