93de65641ac03fd977c269b17ba54991595d5872
takayatomoaki <t.takaya55@gmail.com>
2021-12-08 17:28:55 +0900

    [修正]レイアウト修正

--- resources/views/setup/logout.blade.php
+++ resources/views/setup/logout.blade.php
@@ -7,17 +7,30 @@
         <div class="card">
           <div class="card-header">アカウント設定</div>
 
-          <div class="card-body px-0 py-0">
-            <div class="list-group list-group-flush">
-              <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button type="submit" class="list-group-item list-group-item-action">
-                  ログアウトする
-                </button>
-              </form>
+          <div class="card-body">
+
+            <div class="col my-4 py-1">
+              <h5 class="text-center">ログアウトしますか？</h5>
+            </div>
+            <div class="row text-center my-4">
+              <div class="col">
+                <form action="{{ route('logout') }}" method="POST">
+                  @csrf
+                  <button type="submit" class="btn btn-primary" style="width:130px">
+                    ログアウトする
+                  </button>
+                </form>
+
+              </div>
+              <div class="col">
+                <form action="{{ route('setup', ['user_id'=> $user_id]) }}" method="GET">
+                  <button type="submit" class="btn btn-primary" style="width:130px">
+                    戻る
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
-
 
         </div>
       </div>
